694c7b0ece25dad7e89af061ed4ac743729a3198
Ahnhyojun <hyojunahn111@naver.com>
2023-05-27 18:45:13 +0900

    주별 달력 월별 그림 버튼이랑 액티비티 연결, 메인 xml 수정

--- app/src/main/java/com/example/ggb/MainActivity.java
+++ app/src/main/java/com/example/ggb/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ggb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button weekbtn = (Button) findViewById(R.id.weekbt);
+        weekbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), week.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calbtn = (Button) findViewById(R.id.calbt);
+        calbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), calender.class);
+                startActivity(intent);
+            }
+        });
+
+        Button monthbtn = (Button) findViewById(R.id.monthbt);
+        monthbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), month.class);
+                startActivity(intent);
+            }
+        });
+
+        Button picbtn = (Button) findViewById(R.id.picbt);
+        picbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), picture.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
