806910d8101efb8ea1ccf6dacee011bb90636f44
Ahnhyojun <hyojunahn111@naver.com>
2023-05-28 21:26:21 +0900

    달력 구현, week에 적어둔 db를 달력 안에 넣어보려했으나 안됨, 달력의 달이 제대로 안뜸

---
+++ app/src/main/java/com/example/ggb/CalendarAdapter.java
@@ -0,0 +1,92 @@
+package com.example.ggb;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.ggb.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class CalendarAdapter extends BaseAdapter {
+    private Context context;
+    private ArrayList<Date> dates;
+    private Calendar currentCalendar;
+    private ArrayList<String> expenseList;
+
+    public CalendarAdapter(Context context, ArrayList<Date> dates, Calendar currentCalendar) {
+        this.context = context;
+        this.dates = dates;
+        this.currentCalendar = currentCalendar;
+        this.expenseList = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return dates.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dates.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.calendar_item, parent, false);
+        }
+
+        TextView dayTextView = convertView.findViewById(R.id.dayTextView);
+        TextView expenseTextView = convertView.findViewById(R.id.expenseTextView);
+
+        Date date = dates.get(position);
+        if (date != null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+            dayTextView.setText(String.valueOf(dayOfMonth));
+
+            String expense = null;
+            if (position < expenseList.size()) {
+                expense = expenseList.get(position);
+            }
+
+            if (expense != null) {
+                expenseTextView.setText(expense);
+            } else {
+                expenseTextView.setText("");
+            }
+
+            if (calendar.get(Calendar.MONTH) != currentCalendar.get(Calendar.MONTH)) {
+                dayTextView.setTextColor(ContextCompat.getColor(context, R.color.gray));
+            } else {
+                dayTextView.setTextColor(ContextCompat.getColor(context, R.color.black));
+            }
+        } else {
+            dayTextView.setText("");
+            expenseTextView.setText("");
+        }
+
+        return convertView;
+    }
+
+    public void setExpenseList(ArrayList<String> expenseList) {
+        this.expenseList = expenseList;
+    }
+}
--- app/src/main/java/com/example/ggb/calender.java
+++ app/src/main/java/com/example/ggb/calender.java
@@ -3,15 +3,69 @@ package com.example.ggb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class calender extends AppCompatActivity {
+
+    private GridView calendarGridView;
+    private ArrayList<Date> dates;
+    private CalendarAdapter calendarAdapter;
+    private SimpleDateFormat monthYearFormat;
+    private ArrayList<String> expenseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
 
+        calendarGridView = findViewById(R.id.calendarGridView);
+        TextView monthYearTextView = findViewById(R.id.monthYearTextView);
 
+        dates = new ArrayList<>();
+        monthYearFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
+        expenseList = new ArrayList<>();
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+
+        generateCalendar(calendar);
+
+        calendarAdapter = new CalendarAdapter(this, dates, calendar);
+        calendarAdapter.setExpenseList(expenseList);
+        calendarGridView.setAdapter(calendarAdapter);
+
+        monthYearTextView.setText(monthYearFormat.format(calendar.getTime()));
+    }
+
+    private void generateCalendar(Calendar calendar) {
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        int monthDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        int firstDayOfMonth = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
+        calendar.add(Calendar.DAY_OF_MONTH, -firstDayOfMonth);
+
+        dates.clear();
+        for (int i = 0; i < 42; i++) {
+            dates.add(calendar.getTime());
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+    }
+
+    // 예산 항목을 설정하는 메서드
+    public void setExpenseList(ArrayList<String> expenseList) {
+        this.expenseList = expenseList;
+        if (calendarAdapter != null) {
+            calendarAdapter.setExpenseList(expenseList);
+            calendarAdapter.notifyDataSetChanged();
+        }
     }
 }
