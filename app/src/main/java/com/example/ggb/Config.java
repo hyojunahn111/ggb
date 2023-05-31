762c2f8bc40dfc765a1f43d0a91eaefe8428b27b
Ahnhyojun <hyojunahn111@naver.com>
2023-05-31 20:14:04 +0900

    알림 액티비티랑 다른 액티비티 연결

--- app/src/main/java/com/example/ggb/Config.java
+++ app/src/main/java/com/example/ggb/Config.java
@@ -68,6 +68,47 @@ public class Config extends AppCompatActivity {
             }
         });
 
+        Button weekbtn = (Button) findViewById(R.id.weekbt);
+        weekbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), week.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calbtn = (Button) findViewById(R.id.calbt);
+        calbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), calender.class);
+                startActivity(intent);
+            }
+        });
+
+        Button monthbtn = (Button) findViewById(R.id.monthbt);
+        monthbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), month.class);
+                startActivity(intent);
+            }
+        });
+
+        Button picbtn = (Button) findViewById(R.id.picbt);
+        picbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), picture.class);
+                startActivity(intent);
+            }
+        });
+
+
         // 이하 버튼 및 리스너들은 동일하게 유지합니다.
 
         thresholdEditText = findViewById(R.id.conTxt);
